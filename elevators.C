3e890c3f4ff54042698ba9c0c3c9c5d1e8193c29
Sulejman <suvalic@slu.edu>
2013-10-15 10:55:38 -0500

    stuff sulejman wrote

--- elevators.C
+++ elevators.C
@@ -6,9 +6,66 @@
 #include <iostream>
 #include <pthread.h>
 #include <stdlib.h>
+#include <semaphore.h>
+#include <queue>
+#include <assert.h>
 #include "building.h"
 #include "elevators.h"
+using namespace std;
 
+
+
+
+struct trip{
+  const Person* who;
+  int origin;
+  int destination;
+  sem_t wakeup;
+};
+trip *rider;
+
+class PCQueue {
+public:
+  PCQueue();
+  void enqueue(trip *it);
+  trip *dequeue();
+private:
+  queue<trip *> q;
+  pthread_mutex_t lock; // protects q
+  sem_t Qsize;
+};
+
+PCQueue::PCQueue()
+{
+  pthread_mutex_init(&lock,NULL);
+  sem_init(&Qsize,0,0);
+}
+
+// Add an trip to the queue
+void PCQueue::enqueue(trip *it)
+{
+  pthread_mutex_lock(&lock);
+  q.push(it);
+  pthread_mutex_unlock(&lock);
+  sem_post(&Qsize);
+}
+
+// Sleep until there is an trip on the queue, then return it.
+trip *PCQueue::dequeue()
+{
+  trip *it;
+  sem_wait(&Qsize);
+  pthread_mutex_lock(&lock);
+  assert(!q.empty());
+  it = q.front();
+  q.pop();
+  pthread_mutex_unlock(&lock);
+  return it;
+}
+
+PCQueue myqueue;
+
+//need a queue to put trips on the queue
 //
 // Elevator constructor
 //   Called once for each elevator before the thread is created.
@@ -29,7 +86,15 @@ Elevator::Elevator()
 //
 int Elevator::display_passengers()
 {
-  return 0;
+  rider->who.display();
+  int riders =0;
+  if (door_is_open() && onfloor()==rider->origin){
+    riders++;
+  }
+  //if (door_is_open()
+  //while(sem_wait(&rider->wakeup)){
+    return riders;
+    //}
 }
 
 //
@@ -44,7 +109,20 @@ int Elevator::display_passengers()
 void Elevator::run()
 {
   message("running");
+  int origin, destination;
+  sem_t wakeup;
+  rider=myqueue.dequeue();
+  origin=rider->origin;
+  destination=rider->destination;
+  wakeup=rider->wakeup;
+  if (onfloor()!= origin){
+    move_to_floor(origin);
+  }
   open_door();
+  close_door();
+  move_to_floor(destination);
+  sem_post(&wakeup);
+  //wriet code to dequeue a trip, take floors info and move them
   // Pick up and drop off passengers.
 }
 
@@ -56,7 +134,16 @@ void Elevator::run()
 //
 void take_elevator(const Person *who, int origin, int destination)
 {
-  // Should not return until the person has taken an elevator
+  trip mytrip;
+  mytrip.who=who;
+  mytrip.origin=origin;
+  mytrip.destination=destination;
+  myqueue.enqueue(&mytrip);
+  sem_init(&mytrip.wakeup,0, 0);
+// enqueue "myqueue" &mytrip, elevator 
+  
+  sem_wait(&mytrip.wakeup);
+// Should not return until the person has taken an elevator
   // to their destination floor.
 }
 
